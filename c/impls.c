5fb84fa8d86d4c799e86c908cbc850e1b3b86759
ysthakur <2004yst@gmail.com>
2021-06-19 13:52:27 -0400

    Got rid of methods

--- c/impls.c
+++ c/impls.c
@@ -1,13 +1,4 @@
 #define STACK_SIZE 200
-#define VALS stk->vals
-#define IND stk->ind
-#define STACK Stack *stk
-#define POP() pop(stk)
-#define POPI() popi(stk)
-#define POPD() popd(stk)
-#define POPC() popc(stk)
-#define POPA() popa(stk)
-#define PUSH(val) push(stk, val)
 
 /////////////////Begin utilities and stuff///////////////////
 
@@ -27,7 +18,8 @@ typedef union
   Array *a;
 } Value_Union;
 
-typedef struct {
+typedef struct
+{
   int size;
   Val **arr;
 } Array;
@@ -38,157 +30,152 @@ typedef struct
   Value_Union value;
 } Val;
 
-typedef struct
-{
-  Val *vals[STACK_SIZE];
-  int ind;
-} Stack;
+const Val *vals[STACK_SIZE];
+int ind = 0;
 
-Val *pop(STACK)
-{
-  return VALS[IND--];
-}
+int tempi;
+double tempd;
+char tempc;
+Array *tempa;
+Val *tempv;
 
-void *push(STACK, Val *val) {
-  VALS[++IND] = val;
-}
+#define POP() vals[ind--];
+#define PUSH(val) vals[++ind] = val;
+#define PEEK() vals[ind];
 
-void free_val(Val *val) {
-  if (val->type == ARRAY) {
-    free(val->value.a->arr);
-    free(val->value.a);
+#define FREE_VAL(val)          \
+  {                            \
+    if (val->type == ARRAY)    \
+    {                          \
+      free(val->value.a->arr); \
+      free(val->value.a);      \
+    }                          \
+    free(val);                 \
   }
-  free(val);
-}
 
-int popi(STACK) {
-  Val *popped = pop(stk);
-  int res = popped->value.i;
-  free(popped);
-  return res;
-}
+#define POPI()                \
+  (                           \
+      tempv = POP(),          \
+      tempi = tempv->value.i, \
+      free(tempv),            \
+      tempi)
 
-double popd(STACK) {
-  Val *popped = pop(stk);
-  double res = popped->value.d;
-  free(popped);
-  return res;
-}
+#define POPD()                \
+  (                           \
+      tempv = POP(),          \
+      tempd = tempv->value.d, \
+      free(tempv),            \
+      tempd)
 
-char popc(STACK) {
-  Val *popped = pop(stk);
-  char res = popped->value.c;
-  free(popped);
-  return res;
-}
+#define POPC()                \
+  (                           \
+      tempv = POP(),          \
+      tempc = tempv->value.c, \
+      free(tempv),            \
+      tempc)
 
-Array *popa(STACK) {
-  Val *popped = pop(stk);
-  Array *res = popped->value.a;
-  free(popped);
-  return res;
-}
+#define POPA()                \
+  (                           \
+      tempv = POP(),          \
+      tempa = tempv->value.a, \
+      free(tempv),            \
+      tempa)
 
-Array *make_arr(int len) {
-  Array *arr;
-  arr = malloc(sizeof(Array));
-  arr->size = len;
-  arr->arr = (Val *) calloc(len, sizeof(Value_Union));
-  return arr;
-}
+Array *made_arr;
+#define MAKE_ARR(len)                                     \
+  (                                                       \
+      made_arr = malloc(sizeof(Array)),                        \
+      made_arr->size = len,                                    \
+      made_arr->arr = (Val *)calloc(len, sizeof(Value_Union)), \
+      made_arr)
 
 /////////////////End utilities and stuff///////////////////
 
 /////////////////////Begin stack-related builtins (prefix `~`)/////////////////////
 
-void stk_dup(STACK)
-{
-  VALS[++IND] = VALS[IND];
-}
+#define STK_DUP stk[++ind] = stk[ind];
 
-void stk_swap(STACK)
-{
-  Val *temp = VALS[IND];
-  VALS[IND] = VALS[IND - 1];
-  VALS[IND - 1] = temp;
-}
+#define STK_SWAP()             \
+  {                            \
+    Val *temp = vals[ind];     \
+    vals[ind] = vals[ind - 1]; \
+    vals[ind - 1] = temp;      \
+  }
 
-void stk_rot(STACK)
-{
-  Val *temp = VALS[IND];
-  VALS[IND] = VALS[IND - 2];
-  VALS[IND - 2] = VALS[IND - 1];
-  VALS[IND - 1] = temp;
-}
+#define STK_ROT()                  \
+  {                                \
+    Val *temp = vals[ind];         \
+    vals[ind] = vals[ind - 2];     \
+    vals[ind - 2] = vals[ind - 1]; \
+    vals[ind - 1] = temp;          \
+  }
 
-void stk_irot(STACK)
-{
-  Val *temp = VALS[IND];
-  VALS[IND] = VALS[IND - 1];
-  VALS[IND - 1] = VALS[IND - 2];
-  VALS[IND - 2] = temp;
-}
+#define STK_IROT()                 \
+  {                                \
+    Val *temp = vals[ind];         \
+    vals[ind] = vals[ind - 1];     \
+    vals[ind - 1] = vals[ind - 2]; \
+    vals[ind - 2] = temp;          \
+  }
 
-void stk_over(STACK)
-{
-  IND++;
-  VALS[IND] = VALS[IND - 2];
-}
+#define STK_OVER()             \
+  {                            \
+    ind++;                     \
+    vals[ind] = vals[ind - 2]; \
+  }
 
-void stk_tuck(STACK)
-{
-  Val *temp = VALS[IND - 1];
-  VALS[IND - 1] = VALS[IND];
-  VALS[IND + 1] = VALS[IND];
-  VALS[IND] = temp;
-  IND++;
-}
+#define STK_TUCK()             \
+  {                            \
+    Val *temp = vals[ind - 1]; \
+    vals[ind - 1] = vals[ind]; \
+    vals[ind + 1] = vals[ind]; \
+    vals[ind] = temp;          \
+    ind++;                     \
+  }
 
-void stk_discard(STACK)
-{
-  free(pop(stk));
-}
+#define STK_DISCARD() free(pop(stk));
 
 //////////////////////End stack-related builtins///////////////////////////////
 
 /////////////////////Begin array-related builtins (prefix `:`)/////////////////////
 
 //a b -> a++b
-void arr_append(STACK) {
-  Array *first = pop(stk)->value.a, *second = pop(stk)->value.a;
-  int size1 = first->size, size2 = second->size;
-  Array *res = make_arr(size1 + size2);
-  for (int i = 0; i < size2; i ++) {
-    res->arr[i] = second->arr[i];
+#define ARR_APPEND()                                            \
+  {                                                             \
+    Array *first = POP()->value.a, *second = pop(stk)->value.a; \
+    int size1 = first->size, size2 = second->size;              \
+    Array *res = make_arr(size1 + size2);                       \
+    for (int i = 0; i < size2; i++)                             \
+      res->arr[i] = second->arr[i];                             \
+    for (int i = 0; i < size1; i++)                             \
+      res->arr[i + size2] = first->arr[i];                      \
+    PUSH(res);                                                  \
   }
-  for (int i = 0; i < size1; i ++) {
-    res->arr[i + size2] = first->arr[i];
-  }
-  PUSH(res);
-}
 
 //Pop I, then A, then push A[I]
-void arr_index(STACK) {
-  int i = POPI();
-  Array *a = POPA();
-  PUSH(a->arr[i]);
-}
+#define ARR_INDEX()           \
+  {                           \
+    tempi = POPI();           \
+    tempa = POPA();           \
+    PUSH(tempa->arr[tempi])); \
+  }
 
 //Pop V, then I, then A, then set A[I] = V without pushing anything
-void arr_set(STACK) {
-  Val *v = POP();
-  int i = POPI();
-  Array *a = POPA();
-  a->arr[i] = v;
-}
-
-void arr_make(STACK) {
-  int n = POPI();
-  Array *res = make_arr(n);
-  for (int i = 0; i < n; i ++) {
-    res->arr[i] = POP();
+#define ARR_SET()               \
+  {                             \
+    tempv = POP();              \
+    tempi = POPI();             \
+    tempa = POPA();             \
+    tempa->arr[tempi] = tempv); \
   }
-  PUSH(res);
-}
+
+#define ARR_MAKE()                  \
+  {                                 \
+    tempi = POPI();                 \
+    tempa = MAKE_ARR(tempi);        \
+    for (int i = 0; i < tempi; i++) \
+      tempa->arr[i] = POP();        \
+    PUSH(tempa);                    \
+  }
 
 //////////////////////End array-related builtins///////////////////////////////
